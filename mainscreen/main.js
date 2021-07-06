2b8fbe5c07b8b30956b3fdd217c277834242ed58
Abhishek Dixit <83164668+Abhishek-op@users.noreply.github.com>
2021-07-06 20:41:04 +0530

    Add files via upload

---
+++ mainscreen/main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var pointer = document.getElementById("pointer"),
+	pointerBox = pointer.getBoundingClientRect(),
+	centerPoint = window.getComputedStyle(pointer).transformOrigin,
+	centers = centerPoint.split(" ");
+
+	function rotatePointer(e) {
+		var pointerEvent = e;
+       	if (e.targetTouches && e.targetTouches[0]) {
+          		e.preventDefault(); 
+          		pointerEvent = e.targetTouches[0];
+          		mouseX = pointerEvent.pageX;
+          		mouseY = pointerEvent.pageY;
+    		} else {
+          		mouseX = e.clientX,
+          		mouseY = e.clientY;
+    		}
+
+ var centerY = pointerBox.top + parseInt(centers[1]) - window.pageYOffset,
+ centerX = pointerBox.left + parseInt(centers[0]) - window.pageXOffset,
+ radians = Math.atan2(mouseX - centerX, mouseY - centerY),
+ degrees = (radians * (180 / Math.PI) * -1) + 180; 
+ pointer.style.transform = 'rotate('+degrees+'deg)';
+}
+
+window.addEventListener('mousemove', rotatePointer);
+window.addEventListener('touchmove', rotatePointer);
+window.addEventListener('touchstart', rotatePointer);
+})
